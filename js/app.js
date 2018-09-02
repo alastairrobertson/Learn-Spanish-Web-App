ddf1650f7d543c9163aadb803ba873b847f847e2
Alastair Robertson <ralastair@icloud.com>
2018-09-02 16:33:43 +1000

    added app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//loadApplication is called when the window loads.
+//
+function loadApplication() {
+    console.log("loadApplication() called");
+}
+
+
+
+
+
+
+
+//this ensures elements are loaded and accessible
+window.onload = loadApplication;
