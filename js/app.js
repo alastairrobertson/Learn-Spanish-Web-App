0637208b7bc4b1c033524373ff6bdb0ea03634eb
Alastair Robertson <ralastair@icloud.com>
2018-09-03 20:31:41 +1000

    added working buttons to menu

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 //loadApplication is called when the window loads.
-//
+// It's purpose is to set up the web app for use by showing the menu screen.
+//This method is only called by window.onload
 function loadApplication() {
-    console.log("loadApplication() called");
-    window.currentPage = null;
-
     
+    window.currentPage = null;
     //showMenu
     showMenu();
-    
-    
+    //add menu to document
     document.body.appendChild(currentPage);
+    //style the menu
     menuStyling();
+    //Setup buttons on menu
+    highscoresButtonMenu.addEventListener("click", showHighscores);
+    gameButtonMenu.addEventListener("click", showGame);
+    settingsButtonMenu.addEventListener("click", showSettings);
+    instructionsButtonMenu.addEventListener("click", showInstructions);
+
+
     
 }
 
@@ -24,46 +30,35 @@ function showMenu() {
         menuPage.classList.add("page");
         //menupage contents go here
         header = createCustomElement("HEADER");
-        
-
-
-        welcomeLabel = window.createH1();
+        //create heading
+        welcomeLabel = createH1();
         t = document.createTextNode("Welcome!");
         welcomeLabel.appendChild(t);
         welcomeLabel.id = "welcomeHeader";
         header.appendChild(welcomeLabel);
-
-
-       
-
+        //create spanish flag for the heading
         var img = createImg();
         img.src = "images/spanish_flag.png";
         img.id = "flag";
         
-
+        //add spanish flag to header and then add header to menu
         header.appendChild(img);
         menuPage.appendChild(header);
 
-        menuBody = createCustomElement("menuBody");
+        //create menu body where buttons will go
+        menuBody = createDiv();
+        menuBody.id = "menuBody";
+        //create each button on the menu
         for (i = 0; i < menuButtons.length; i++) {
-            console.log("does this get run");
-            button = createCustomElement("BUTTON");
+            
+            button = createButton();
             var t = document.createTextNode(menuButtons[i]);
             button.appendChild(t);
             button.id = menuButtons[i];
             menuBody.appendChild(button);
         }
         menuPage.appendChild(menuBody);
-
-
         
-        
-
-        
-        
-        
-       
-
         
     }
     //set the new page
@@ -95,6 +90,16 @@ function showHighscores() {
 
     //show the page
     currentPage.classList.remove("hide");
+}
+
+function showGame() {
+
+}
+function showInstructions() {
+
+}
+function showSettings() {
+
 }
 
 
--- js/styling.js
+++ js/styling.js
@@ -1,15 +1,57 @@
+
+
+
+//menuStyling()
+//This method 
+
 window.menuStyling = function() {
     var flag = this.document.getElementById("flag");
     var welcomeHeader = this.document.getElementById("welcomeHeader");
+    var buttons = this.document.getElementById("menuBody");
+    window.instructionsButtonMenu = this.document.getElementById("Instructions");
+    window.gameButtonMenu = this.document.getElementById("Game");
+    window.highscoresButtonMenu = this.document.getElementById("High Scores");
+    window.settingsButtonMenu = this.document.getElementById("Settings");
+    
 
 
-    flag.style.width = "100px";
+    //flag styling
+    flag.style.width = "300px";
     flag.style.height = "auto";
     flag.style.display = "block";
     flag.style.marginLeft = "auto";
     flag.style.marginRight = "auto";
 
+    //welcome header styling
     welcomeHeader.style.textAlign = "center";
+
+    //general button styling 
+    
+    buttons.style.width = "300px";
+    buttons.style.height = "100px";
+    buttons.style.display = "grid";
+    buttons.style.marginLeft = "auto";
+    buttons.style.marginRight = "auto";
+
+    
+    
+    
+
+
+    
+    
+
+
 
 
 }
+
+
+
+window.highscoresStyling = function() {
+
+
+
+
+}
+
